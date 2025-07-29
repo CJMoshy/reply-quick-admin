19d1d4569dae5e117c5b70085d6b8a6945fda66c
CJ Moshy <cjmoshy@ucsc.edu>
2025-07-29 11:54:04 -0700

    add in basic auth with existing supabase db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-
+import { signOut } from "@/utils/actions/actions";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
@@ -7,6 +7,9 @@ export default function Home() {
       <Link href={"/number"} className="text-blue-500 hover:underline">
         Manage Phone Numbers
       </Link>
+      <button onClick={signOut} className="mt-4 bg-red-500 text-white px-4 py-2 rounded">
+        Sign Out
+      </button>
     </div>
   );
 }
